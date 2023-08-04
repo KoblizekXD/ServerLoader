a1a03bb2ee5be7e16c49a6add3a8f977bfd28472
KoblizekXD <janprokupek@centrum.cz>
2023-08-04 20:01:06 +0200

    Added support for global gradle plugin on-repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,15 @@ plugins {
     kotlin("jvm") version "1.9.0"
     `maven-publish`
     `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "1.1.0"
 }
 
 group = "lol.koblizek"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
@@ -24,14 +26,6 @@ tasks.test {
 kotlin {
     jvmToolchain(8)
 }
-gradlePlugin {
-    plugins {
-        create("serverloader") {
-            id = "serverloader"
-            implementationClass = "lol.koblizek.serverloader.plugin.ServerLoaderPlugin"
-        }
-    }
-}
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
@@ -42,5 +36,19 @@ publishing {
     }
     repositories {
         mavenLocal()
+    }
+}
+gradlePlugin {
+    website = "https://github.com/KoblizekXD/ServerLoader"
+    vcsUrl = "https://github.com/KoblizekXD/ServerLoader"
+
+    plugins {
+        create("serverloader") {
+            id = "lol.koblizek.serverloader"
+            displayName = "Server Loader"
+            description = "Gradle plugin for quick Minecraft server setup"
+            tags.set(listOf("minecraft", "server", "papermc", "plugins"))
+            implementationClass = "lol.koblizek.serverloader.plugin.ServerLoaderPlugin"
+        }
     }
 }
